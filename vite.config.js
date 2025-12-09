88b54ebf5b19c8097bb303a717d53c387d61c38d
Solomon Yahwin <solomonyahwin@gmail.com>
2025-12-09 15:35:53 +0100

    Updated vite.config.js
    Missed the slashes when i values the base option

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Result-Summary-Component',
+  base: '/Result-Summary-Componen/t',
 })
